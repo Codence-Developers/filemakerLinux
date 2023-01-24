ad949b0e3d4ad20d71075de9b4a98143756fa482
justin-pfefferle <justin.pfefferle@codence.com>
2023-01-24 12:05:08 -0700

    Comments

--- launch.sh
+++ launch.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+# This launch script minimizes the user data needed when launching a new instance.
+# Required arguments: ElasticIP FQDN SSL_Email FMS_License Admin_Password Admin_PIN
+# Arguments must be provided in the specified order.
+
 apt install unzip -y
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
@@ -7,3 +12,9 @@ aws ec2 associate-address --instance-id $(curl -s http://169.254.169.254/latest/
 wget 'https://raw.githubusercontent.com/Codence-Developers/filemakerLinux/main/fmsLaunch.sh' -O /root/fmsLaunch.sh
 chmod +x /root/fmsLaunch.sh
 /root/fmsLaunch.sh ${2} ${3} ${4} ${5} ${6}
+
+# User Data:
+#   #!/bin/bash
+#   wget 'https://raw.githubusercontent.com/Codence-Developers/filemakerLinux/main/launch.sh' -O /root/launch.sh
+#   chmod +x /root/launch.sh
+#   /root/launch.sh <ElasticIP> <FQDN> <SSL_Email> <FMS_License> <Admin_Password> <Admin_PIN>
